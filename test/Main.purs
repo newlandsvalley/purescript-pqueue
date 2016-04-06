2114450831ae8ea054c1dff884808b47e69dc4d9
Josh Bassett <josh.bassett@gmail.com>
2016-04-06 21:03:16 +1000

    Update purescript-spec dep.

--- test/Main.purs
+++ test/Main.purs
@@ -4,13 +4,14 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
-import Test.Spec.Runner (Process, run)
+import Node.Process (PROCESS())
+import Test.Spec.Runner (run)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 import Test.Data.PQueue (pqueueSpec)
 import Test.Data.PQueue.Partial (partialSpec)
 
-main :: forall e. Eff (console :: CONSOLE, process :: Process | e) Unit
+main :: forall e. Eff (console :: CONSOLE, process :: PROCESS | e) Unit
 main = run [consoleReporter] do
   pqueueSpec
   partialSpec
