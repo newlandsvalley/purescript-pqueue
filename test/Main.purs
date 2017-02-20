4cd5ab1569e9de8a13805bbda54cea84b1426193
Josh Bassett <josh.bassett@gmail.com>
2017-02-20 21:18:57 +1100

    Update deps

--- test/Main.purs
+++ test/Main.purs
@@ -3,15 +3,13 @@ module Test.Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Node.Process (PROCESS())
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (RunnerEffects, run)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 import Test.Data.PQueue (pqueueSpec)
 import Test.Data.PQueue.Partial (partialSpec)
 
-main :: forall e. Eff (console :: CONSOLE, process :: PROCESS | e) Unit
+main :: Eff (RunnerEffects ()) Unit
 main = run [consoleReporter] do
   pqueueSpec
   partialSpec
