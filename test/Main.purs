a36a4fdb45f056431e03d7cf2d30f180bb882c18
John Watson <john.watson@gmx.co.uk>
2022-01-14 10:04:59 +0000

    Update bower.json to PS 0.14, add spago

--- test/Main.purs
+++ test/Main.purs
@@ -2,14 +2,14 @@ module Test.Main where
 
 import Prelude
 
-import Effect (Effect)
-import Test.Spec.Runner (run)
+import Effect.Aff (Aff)
+import Test.Spec.Runner (run, runSpec)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 import Test.Data.PQueueSpec (pqueueSpec)
 import Test.Data.PQueue.PartialSpec (partialSpec)
 
-main :: Effect Unit
-main = run [consoleReporter] do
+main :: Aff Unit
+main = runSpec [consoleReporter] do
   pqueueSpec
   partialSpec
