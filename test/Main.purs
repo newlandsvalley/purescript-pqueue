7d96031505c000026570c5b2222dab08e6518a88
John Watson <john.watson@gmx.co.uk>
2022-01-14 15:02:17 +0000

    Convert tests to run in Aff

--- test/Main.purs
+++ test/Main.purs
@@ -2,14 +2,17 @@ module Test.Main where
 
 import Prelude
 
-import Effect.Aff (Aff)
+import Effect (Effect)
+import Effect.Aff (launchAff)
 import Test.Spec.Runner (run, runSpec)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 import Test.Data.PQueueSpec (pqueueSpec)
 import Test.Data.PQueue.PartialSpec (partialSpec)
 
-main :: Aff Unit
-main = runSpec [consoleReporter] do
-  pqueueSpec
-  partialSpec
+main :: Effect Unit
+main = do 
+  _ <- launchAff $ runSpec [consoleReporter] do
+         pqueueSpec
+         partialSpec
+  pure unit
